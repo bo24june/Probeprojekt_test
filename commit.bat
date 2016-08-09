3f78734184782e8113a9f176c127704756a400c9
root <timo.herzog@fau.de>
2016-08-09 15:05:06 +0200

    testerino

--- commit.bat
+++ commit.bat
@@ -4,8 +4,7 @@ read -p "Commit description: " desc
 git commit -m "$desc"  
 git push origin master
 
+git clone https://github.com/bo24june/Probeprojekt_test.git
 
-
-git pull https://bo24june:Rew21121996t@github.com/bo24june/Probeprojekt_test.git
 
 
