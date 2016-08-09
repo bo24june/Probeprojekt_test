a169754b9b0b47d58b3ac7b60632d82df9a7e4fb
root <timo.herzog@fau.de>
2016-08-09 14:54:56 +0200

    test3

--- commit.bat
+++ commit.bat
@@ -3,3 +3,9 @@ git add .
 read -p "Commit description: " desc  
 git commit -m "$desc"  
 git push origin master
+
+
+
+git pull https://bo24june:Rew21121996t@github.com/bo24june/Probeprojekt_test.git
+
+
