5bc68974354f7a8af6b46ccd09895b885e0e27cb
root <timo.herzog@fau.de>
2016-08-09 15:13:48 +0200

    test14

--- commit.bat
+++ commit.bat
@@ -5,7 +5,24 @@ git commit -m "$desc"
 git push origin master 
 
 
-git clone https://github.com/bo24june/Probeprojekt_test/tree/cisco
+#git clone https://github.com/bo24june/Probeprojekt_test/tree/cisco 
+
+
+ Clone just the repository's .git folder (excluding files as they are already in
+# `existing-dir`) into an empty temporary directory
+git clone --no-checkout https://github.com/bo24june/Probeprojekt_test/tree/cisco srv/git/cisco_conf_test/cisco/srv/git/cisco_conf_test/cisco.tmp # might want --no-hardlinks for cloning local repo
+
+# Move the .git folder to the directory with the files.
+# This makes `existing-dir` a git repo.
+mv srv/git/cisco_conf_test/cisco/srv/git/cisco_conf_test/cisco.tmp/.git srv/git/cisco_conf_test/cisco/
+
+# Delete the temporary directory
+rmdir srv/git/cisco_conf_test/cisco/srv/git/cisco_conf_test/cisco.tmp
+cd srv/git/cisco_conf_test/cisco
+
+# git thinks all files are deleted, this reverts the state of the repo to HEAD.
+# WARNING: any local changes to the files will be lost.
+git reset --hard HEAD
 
 
 
