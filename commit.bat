c7a9ff9ff73f885722ef705e229d609a45faeac8
root <timo.herzog@fau.de>
2016-08-09 15:06:55 +0200

    testerino1

--- commit.bat
+++ commit.bat
@@ -2,9 +2,10 @@
 git add .  
 read -p "Commit description: " desc  
 git commit -m "$desc"  
-git push origin master
+git push origin master 
 
-git clone https://github.com/bo24june/Probeprojekt_test.git
+
+git clone https://github.com/bo24june/Probeprojekt_test/tree/cisco
 
 
 
