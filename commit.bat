fe5c362594aa5ea4d3825d619db398c2971c2783
root <timo.herzog@fau.de>
2016-08-10 10:11:13 +0200

    123

--- commit.bat
+++ commit.bat
@@ -1,9 +1,10 @@
 #!/bin/bash
 git add .  
 read -p "Commit description: " desc  
+git config --global credential.helper "cache --timeout=84400"
 git commit -m "$desc"
 git pull  
-git push origin master 
+git push origin cisco_konfiguration
 
 #git clone bo24june@github.com:bo24june/Probeprojekt_test.git temp
 #mv temp/.git  cisco_conf_test/.git
