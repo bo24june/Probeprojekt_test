488a9df5da1a1f486a76d23e2d83b0233640e0cf
root <timo.herzog@fau.de>
2016-08-10 08:40:09 +0200

    123

--- commit.bat
+++ commit.bat
@@ -5,6 +5,6 @@ git commit -m "$desc"
 git push origin master 
 
 
-#git clone https://github.com/bo24june/Probeprojekt_test.git temp
+#git clone bo24june@github.com:bo24june/Probeprojekt_test.git temp
 mv temp/.git  cisco_conf_test/.git
 rm -rf temp 
