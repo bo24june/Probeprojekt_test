4fccb63e7b4cc8aa42a05bb7d912af429c68910f
testuser <timo.herzog@fau.de>
2016-08-11 10:57:13 +0200

    Cron-Job_aktualisierung

--- commit.sh
+++ commit.sh
@@ -1,34 +1,14 @@
 #/bin/bash
 
-#rsync -rPz /srv/ftp/testuser/ /srv/git/cisco/cisco_conf_test/testuser/
-
-#cp -R /srv/ftp/testuser/ /srv/git/cisco/cisco_conf_test/
-
 rsync -avh /srv/ftp/testuser/ /srv/git/cisco/cisco_conf_test/testuser/  --delete
 
-
-
-   # It will copy only incremental data.
-   # It will delete if any data deleted from source.
-   # It will copy again from source if any data deleted at destination.
-   # basically this command will keep the both environment in sync.
-
-#rsync -avWe --delete-before /srv/ftp/testuser/ /srv/git/cisco/cisco_conf_test/testuser/
-
-#rsync -avW --delete-before -e  /srv/ftp/testuser/ /srv/git/cisco/cisco_conf_test/testuser/
-
-
- git add . 
+git add . 
  
 git config --global credential.helper "cache --timeout=84400"
 git commit -m "Cron-Job_aktualisierung"
 git pull  
 git push origin master
 
- 
-
-
-##rsync -rPz  /srv/git/cisco/cisco_conf_test/testuser/  /srv/ftp/testuser/ 
 
  rsync -avW --delete-before -e  /srv/ftp/testuser/ /srv/git/cisco/cisco_conf_test/testuser/
 
